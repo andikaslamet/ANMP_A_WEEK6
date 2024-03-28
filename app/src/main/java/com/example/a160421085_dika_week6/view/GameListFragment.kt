0c2c9a147e50bad2825179d6c762becd74a286fd
acer <s160421085@student.ubaya.ac.id>
2024-03-28 15:30:50 +0700

    update activity main,android manifest,gradle build,gamelistfragment,gamenav,listviewmodel

--- app/src/main/java/com/example/a160421085_dika_week6/view/GameListFragment.kt
+++ app/src/main/java/com/example/a160421085_dika_week6/view/GameListFragment.kt
@@ -5,15 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a160421085_dika_week6.R
 import com.example.a160421085_dika_week6.databinding.FragmentGameListBinding
+import com.example.a160421085_dika_week6.viewmodel.ListViewModel
 
 class GameListFragment : Fragment() {
+    private lateinit var viewModel: ListViewModel
+    private val studentListAdapter  = GameListAdapter(arrayListOf())
     private lateinit var binding:FragmentGameListBinding
     override fun onCreateView(inflater: LayoutInflater, container:
     ViewGroup?,savedInstanceState: Bundle?): View? {
         binding = FragmentGameListBinding.inflate(inflater, container, false)
         return binding.root
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = studentListAdapter
+
+        observeViewModel()
+    }
+    fun observeViewModel() {
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer{
+            if(it == true) {
+                binding.recyclerView.visibility = View.GONE
+                binding.progressBar2.visibility = View.VISIBLE
+            } else {
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.progressBar2.visibility = View.GONE
+            }
+        })
+
+    }
 }
--- app/src/main/java/com/example/a160421085_dika_week6/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/a160421085_dika_week6/viewmodel/ListViewModel.kt
@@ -1,4 +1,56 @@
 package com.example.a160421085_dika_week6.viewmodel
 
-class ListViewModel {
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.a160421085_dika_week6.model.Game
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class ListViewModel(application: Application):AndroidViewModel(application) {
+    val GameLD = MutableLiveData<ArrayList<Game>>()
+    val GameLoadErrorLD = MutableLiveData<Boolean>()
+    val loadingLD = MutableLiveData<Boolean>()
+
+    val TAG = "volleyTag"
+    private var queue: RequestQueue? = null
+
+    fun refresh() {
+        GameLoadErrorLD.value = false
+        loadingLD.value = true
+
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "http://10.0.2.2/Game/Game.json"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            {
+                val sType = object : TypeToken<List<Game>>() { }.type
+                val result = Gson().fromJson<List<Game>>(it, sType)
+                GameLD.value = result as ArrayList<Game>?
+                loadingLD.value = false
+
+                Log.d("show_volley", it)
+            },
+            {
+                Log.e("show_volley", it.toString())
+                GameLoadErrorLD.value = false
+                loadingLD.value = false
+            })
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        queue?.cancelAll(TAG)
+    }
 }
